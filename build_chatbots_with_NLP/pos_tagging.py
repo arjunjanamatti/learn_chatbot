59e1efa6aa94f220f00c189025f8fd479445f52c
arjunjanamatti <arjunjanamatti@gmail.com>
2021-03-08 12:02:08 +0530

    update with class instance and inheritance

--- build_chatbots_with_NLP/pos_tagging.py
+++ build_chatbots_with_NLP/pos_tagging.py
@@ -1,40 +1,5 @@
 import spacy
 
-nlp = spacy.load('en_core_web_sm')
-
-doc = nlp(text='Checking on the parts of speech and tokenization')
-n = 10
-for token in doc:
-    print(token.text,'\t', token.lemma_,'\t', token.pos_,'\t',
-          token.tag_,'\t', token.dep_,'\t',token.shape_,'\t',
-          token.is_alpha,'\t', token.is_stop)
-
-
-# TEXT Actual text or word being processed
-# LeMMa root form of the word being processed
-# POS part-of-speech of the wordtagthey express the part-of-speech (e.g., Verb) and some amount of morphological information (e.g., that the verb is past tense).
-# Dep syntactic dependency (i.e., the relation between tokens)
-# Shape shape of the word (e.g., the capitalization, punctuation, digits format)
-# Alpha is the token an alpha character?
-# Stop is the word a stop word or part of a stop list?
-
-
-#### NAMED ENTITY RECOGNITION
-test_string = u"Google has its headquarters in Mountain View, California having revenue amounted to 109.65 billion US dollars"
-doc_ner = nlp(text=test_string)
-
-my_string = u"Mark Zuckerberg born May 14, 1984 in New York is an American technology entrepreneur and philanthropist best known for co-founding and leading Facebook as its chairman and CEO."
-doc = nlp(my_string)
-for ent in doc.ents:
-    print(ent.text, ent.label_)
-
-
-
-print('#### NAMED ENTITY RECOGNITION')
-for entities in doc_ner.ents:
-    print(entities.text, entities.label_)
-
-
 class using_spacy:
     def __init__(self, model, text):
         self.model = model
@@ -44,6 +9,7 @@ class using_spacy:
 
 
     def PartsofSpeechTagging(self):
+        print('#### PARTS OF SPEECH')
         for token in self.doc:
             print(token.text, '\t', token.lemma_, '\t', token.pos_, '\t',
                   token.tag_, '\t', token.dep_, '\t', token.shape_, '\t',
@@ -51,8 +17,26 @@ class using_spacy:
 
     def NamedEntityRecognition(self):
         print('#### NAMED ENTITY RECOGNITION')
-        for entities in doc_ner.ents:
+        for entities in self.doc.ents:
             print(entities.text, entities.label_)
         pass
 
     pass
+
+model_1 = 'en_core_web_sm'
+model_2 = 'en_core_web_trf'
+text_1 = 'Checking on the parts of speech and tokenization'
+text_2 = "Google has its headquarters in Mountain View, California having revenue amounted to 109.65 billion US dollars"
+text_3 = "Mark Zuckerberg born May 14, 1984 in New York is an American technology entrepreneur and philanthropist best known for co-founding and leading Facebook as its chairman and CEO."
+
+instance_1 = using_spacy(model_1, text_1)
+instance_1.PartsofSpeechTagging()
+
+instance_2 = using_spacy(model_2, text_2)
+instance_2.NamedEntityRecognition()
+
+instance_3 = using_spacy(model_1, text_3)
+instance_3.NamedEntityRecognition()
+
+instance_3 = using_spacy(model_2, text_3)
+instance_3.NamedEntityRecognition()
