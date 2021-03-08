925953724ea56332d5428b8e9d8e69848b347b8b
arjunjanamatti <arjunjanamatti@gmail.com>
2021-03-08 14:38:47 +0530

    first commit for RASA NLU

--- build_chatbots_with_NLP/pos_tagging.py
+++ build_chatbots_with_NLP/pos_tagging.py
@@ -31,9 +31,19 @@ class using_spacy:
             print(chunk.text, chunk.root.text, chunk.root.dep_, chunk.root.head.text)
         pass
 
+    def SentencesSimilarity(self, sent_1, sent_2):
+        '''
+        spaCy uses high-quality word vectors to find similarity between two words using GloVe algorithm (Global Vectors for Word Representation).
+        '''
+        sent_1_doc = self.nlp(sent_1)
+        sent_2_doc = self.nlp(sent_2)
+        print(sent_1_doc.similarity(sent_2_doc))
+
+        pass
+
     pass
 
-model_1 = 'en_core_web_sm'
+model_1 = 'en_core_web_lg'
 model_2 = 'en_core_web_trf'
 text_1 = 'Checking on the parts of speech and tokenization'
 text_2 = "Google has its headquarters in Mountain View, California having revenue amounted to 109.65 billion US dollars"
@@ -48,5 +58,6 @@ text_3 = "Mark Zuckerberg born May 14, 1984 in New York is an American technolog
 # instance_3 = using_spacy(model_1, text_3)
 # instance_3.NamedEntityRecognition()
 #
-instance_3 = using_spacy(model_2, text_3)
-instance_3.NounChunks()
+instance_3 = using_spacy(model_1, text_3)
+instance_3.SentencesSimilarity('I am having Tea', 'I want to have Tea')
+
