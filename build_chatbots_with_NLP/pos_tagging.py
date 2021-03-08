db8b99e7434bd0ca9d15b3279943de90ed08a45a
arjunjanamatti <arjunjanamatti@gmail.com>
2021-03-08 12:22:41 +0530

    update with display dependency parser

--- build_chatbots_with_NLP/pos_tagging.py
+++ build_chatbots_with_NLP/pos_tagging.py
@@ -1,4 +1,5 @@
 import spacy
+from spacy import displacy
 
 class using_spacy:
     def __init__(self, model, text):
@@ -21,6 +22,10 @@ class using_spacy:
             print(entities.text, entities.label_)
         pass
 
+    def DisplayDependencyParsing(self):
+        displacy.serve(self.doc, style='dep')
+        pass
+
     pass
 
 model_1 = 'en_core_web_sm'
@@ -29,14 +34,15 @@ text_1 = 'Checking on the parts of speech and tokenization'
 text_2 = "Google has its headquarters in Mountain View, California having revenue amounted to 109.65 billion US dollars"
 text_3 = "Mark Zuckerberg born May 14, 1984 in New York is an American technology entrepreneur and philanthropist best known for co-founding and leading Facebook as its chairman and CEO."
 
-instance_1 = using_spacy(model_1, text_1)
-instance_1.PartsofSpeechTagging()
-
-instance_2 = using_spacy(model_2, text_2)
-instance_2.NamedEntityRecognition()
-
-instance_3 = using_spacy(model_1, text_3)
-instance_3.NamedEntityRecognition()
-
+# instance_1 = using_spacy(model_1, text_1)
+# instance_1.PartsofSpeechTagging()
+#
+# instance_2 = using_spacy(model_2, text_2)
+# instance_2.NamedEntityRecognition()
+#
+# instance_3 = using_spacy(model_1, text_3)
+# instance_3.NamedEntityRecognition()
+#
 instance_3 = using_spacy(model_2, text_3)
 instance_3.NamedEntityRecognition()
+instance_3.DisplayDependencyParsing()
